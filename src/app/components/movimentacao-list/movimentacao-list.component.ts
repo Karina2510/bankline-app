dbe4fcfb1af1c12d59de7341dd638977363f5b60
Karina Ferreira <96082465+Karina2510@users.noreply.github.com>
2022-05-09 23:25:48 -0300

    atualizações

--- src/app/components/movimentacao-list/movimentacao-list.component.ts
+++ src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CorrentistaService } from 'src/app/services/correntista.service';
+import { MovimentacaoService } from 'src/app/services/movimentacao.service';
 
 @Component({
   selector: 'app-movimentacao-list',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movimentacao-list.component.css']
 })
 export class MovimentacaoListComponent implements OnInit {
-
-  constructor() { }
+movimentacoes:any;
+correntistas:any;
+correntista:any;
+  constructor(
+    private movimentacaoService: MovimentacaoService, 
+    private CorrentistaService: CorrentistaService) { }
 
   ngOnInit(): void {
+    this.exibirCorrentistas();
   }
-
-}
+  exibirCorrentistas(): void {
+    this.CorrentistaService.list()
+      .subscribe(
+        data => {
+          this.correntistas = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+  listMovimentacoes(): void{
+    this.movimentacaoService.findyByIdConta(this.correntista.id)
+    .subscribe(
+      data => {
+        this.movimentacoes = data;
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      });
+    }
+  
+  }
